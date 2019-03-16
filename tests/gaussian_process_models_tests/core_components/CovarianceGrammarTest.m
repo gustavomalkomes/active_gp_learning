88d17df9d9b20b6604fa4759f020fcfce3f8b5d6
Gustavo Malkomes <gustavo.muniz@gmail.com>
2019-03-15 19:28:23 -0500

    adding full_expand

--- tests/gaussian_process_models_tests/core_components/CovarianceGrammarTest.m
+++ tests/gaussian_process_models_tests/core_components/CovarianceGrammarTest.m
@@ -130,20 +130,26 @@ classdef CovarianceGrammarTest < matlab.unittest.TestCase
             dim = 2;
             grammar = CovarianceGrammar(root, dim, []);
             
+            max_number_of_models = 1000;
             level = 0;
-            covariances = grammar.full_expand(level);
+            covariances = grammar.full_expand(level, max_number_of_models);
             expected = numel(root)*dim;
             testCase.assertEqual(expected, numel(covariances));
             
-
             level = 1;
-            covariances = grammar.full_expand(level);
-            expected = (numel(root)*dim+1)*numel(root)*dim;
+            covariances = grammar.full_expand(level, max_number_of_models);
+            n = numel(root);
+            expected = (n*dim+1)*n*dim;
             testCase.assertEqual(expected, numel(covariances));
             
             level = 2;
-            covariances = grammar.full_expand(level);
+            covariances = grammar.full_expand(level, max_number_of_models);
             expected = 134;
+            testCase.assertEqual(expected, numel(covariances));
+
+            level = 4;
+            covariances = grammar.full_expand(level, max_number_of_models);
+            expected = max_number_of_models;
             testCase.assertEqual(expected, numel(covariances));
         end
     end
